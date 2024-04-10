d8769b27456b690532dd5c4214034916adc8fe37
agileahn2 <50093953+agileahn2@users.noreply.github.com>
2024-04-10 23:46:22 +0900

    Title: 메이즈 러너, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240410/메이즈 러너/maze-runner.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int N, M, K;
+        BufferedReader br;
+        StringTokenizer st;
+
+
+
+
+
+    }
+}
