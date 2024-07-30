fc5b56fad87b097632416f60402502947a5e6713
agileahn2 <50093953+agileahn2@users.noreply.github.com>
2024-07-30 23:35:43 +0900

    Title: 최고의 33위치, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/최고의 33위치/best-place-of-33.py
+++ 240730/최고의 33위치/best-place-of-33.py
@@ -3,18 +3,17 @@ grid = [list(map(int, input().split()))for _ in range(N)]
 
 Max = 0
 Temp = 0
-count = 0
-loop = (N - 2)*(N - 2) # - 3 + 1
-while(True):
-    if count == loop:
-        break
-    for i, g in zip(range(N), grid):
-        for n, j in enumerate(g):
-            if n > N + i:
-                break
-            Temp += j
-    if Temp > Max:
-        Max = Temp
-    Temp = 0
-    count += 1
+loop = (N - 2) # - 3 + 1
+
+for I in range(loop):
+    for J in range(loop):
+        for i in range(I, 3+I):
+            for j in range(J, 3+J):
+                Temp += grid[i][j]
+
+        if Max < Temp:
+            Max = Temp
+        Temp = 0
+
+
 print(Max)
