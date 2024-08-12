2167ad8a54d2ac67d0a8125fe08f942045976e11
agileahn2 <50093953+agileahn2@users.noreply.github.com>
2024-08-12 22:26:27 +0900

    Title: 1차원 젠가, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/1차원 젠가/jenga-1d.py
+++ 240812/1차원 젠가/jenga-1d.py
@@ -1,20 +1,26 @@
 from sys import stdin, stdout
 n = int(stdin.readline()) #6
 
+def oneToTow(x):
+    if len(x) == 1:
+        return "0"+x
+    else:
+        return x
+
 #문자열로 입력받기
 arr = ""
 for _ in range(n):
-    arr += input()
+    arr += oneToTow(input())
 
 
 delete = [list(map(int, stdin.readline().split())) for _ in range(2)]   # 삭제할 인덱스 2개
 
 #파이썬의 문자열 슬라이싱 활용
 for s, e in delete:
-    arr = arr[:s-1] + arr[e:]
+    arr = arr[:s*2-2] + arr[e*2:]
 
 #개수 출력
-print(len(arr))
+print(int(len(arr)/2))
 #남은 젠가 출력
-for i in range(len(arr)):
-    print(arr[i])
+for i in range(0, len(arr), 2):
+    print(int(arr[i]) + int(arr[i+1]))
