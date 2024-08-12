825b97bc765286d276dfe772998772a8b8fef0e4
agileahn2 <50093953+agileahn2@users.noreply.github.com>
2024-08-12 22:34:41 +0900

    Title: 1차원 젠가, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/1차원 젠가/jenga-1d.py
+++ 240812/1차원 젠가/jenga-1d.py
@@ -3,6 +3,8 @@ n = int(stdin.readline()) #6
 
 def oneToTow(x):
     if len(x) == 1:
+        return "00"+x
+    elif len(x) == 2:
         return "0"+x
     else:
         return x
@@ -17,10 +19,10 @@ delete = [list(map(int, stdin.readline().split())) for _ in range(2)]   # 삭제
 
 #파이썬의 문자열 슬라이싱 활용
 for s, e in delete:
-    arr = arr[:s*2-2] + arr[e*2:]
+    arr = arr[:s*3-3] + arr[e*3:]
 
 #개수 출력
-print(int(len(arr)/2))
+print(int(len(arr)/3))
 #남은 젠가 출력
-for i in range(0, len(arr), 2):
-    print(int(arr[i]+"0") + int(arr[i+1]))
+for i in range(0, len(arr), 3):
+    print(int(arr[i]+arr[i+1]+arr[i+2]))
